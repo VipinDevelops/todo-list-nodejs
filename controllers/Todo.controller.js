951330471bc608acb30e90a1d70048e7058b2a6c
imvipindev <vipinchaudhary@gmail.com>
2022-11-18 20:01:39 +0530

    feat(todo.controller): added delete Todo

--- controllers/Todo.controller.js
+++ controllers/Todo.controller.js
@@ -27,7 +27,6 @@ const getTodo = (req, res) => {
 };
 
 const postTodo = (req, res) => {
-  //Get the id of last items
   let itemIds = Todos.map((item) => item.id);
 
   let newId = itemIds.length > 0 ? Math.max(...itemIds) + 1 : 1;
@@ -41,8 +40,39 @@ const postTodo = (req, res) => {
   res.status(201).json(newItem);
 };
 
+const putTodo = (req, res) => {
+  let found = Todos.find((item) => {
+    return item.id == parseInt(req.params.id);
+  });
+  if (found) {
+    let updated = {
+      id: found.id,
+      title: req.body.title,
+      completed: req.body.completed,
+    };
+    let targetIndex = Todos.indexOf(found);
+    Todos.splice(targetIndex, 1, updated);
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+const deleteTodo = (req, res) => {
+  let found = Todos.find((item) => {
+    return item.id == parseInt(req.params.id);
+  });
+
+  if (found) {
+    let targetIndex = Todos.indexOf(found);
+    Todos.splice(targetIndex, 1);
+  }
+  res.sendStatus(204);
+};
 module.exports = {
   getTodos,
   getTodo,
   postTodo,
+  putTodo,
+  deleteTodo,
 };
