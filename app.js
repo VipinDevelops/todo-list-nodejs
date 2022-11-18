f0f2c6ce2b5334deda2e2462463486f20bfef145
imvipindev <vipinchaudhary@gmail.com>
2022-11-18 20:01:10 +0530

    chore(app):changed log statement of server

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(express.json());
 
 app.use("/api", TodoRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello API is now Working");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello There API is now Working");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is now Started on http://${host}:${PORT}`);
