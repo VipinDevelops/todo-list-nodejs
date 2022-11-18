0a732d28bfd7053b25c1bf068ca32ec552ea4f7c
imvipindev <vipinchaudhary@gmail.com>
2022-11-18 18:48:07 +0530

    feat(todo.controller):added get Todos and Todo

--- controllers/Todo.controller.js
+++ controllers/Todo.controller.js
@@ -1,7 +1,33 @@
+const Todos = [
+  {
+    id: 1,
+    title: "Study",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Coding",
+    completed: true,
+  },
+];
+
 const getTodos = (req, res) => {
-  res.send("get Todos from controller");
+  res.status(201).json(Todos);
+};
+
+const getTodo = (req, res) => {
+  let found = Todos.find(function (item) {
+    return item.id === parseInt(req.params.id);
+  });
+
+  if (found) {
+    res.status(200).json(found);
+  } else {
+    res.sendStatus(404);
+  }
 };
 
 module.exports = {
   getTodos,
+  getTodo,
 };
