8bcf08906ece54248302db039c0280603b61490c
imvipindev <vipinchaudhary@gmail.com>
2022-11-18 17:45:14 +0530

    feat(models): added model for Todo

---
+++ models/Todo.model.js
@@ -0,0 +1,11 @@
+class Todo {
+  id;
+  title;
+  description;
+  constructor(id, title, description) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+  }
+}
+module.exports = Task;
