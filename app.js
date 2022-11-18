39539a3b0a0c71a91fcbda00339dd14dd51209a4
imvipindev <vipinchaudhary@gmail.com>
2022-11-18 17:25:30 +0530

    chore(app.js): added middlwares

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
-
+const TodoRoutes = require("./routes/Todo.route");
 const app = express();
-
 const PORT = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello API is now Working");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api", TodoRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello API is now Working");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is now Started on Port ${PORT}`);
