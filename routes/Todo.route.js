4d1bb64f48b2f3cd0e694d12aeaf150a80e9bea7
imvipindev <vipinchaudhary1809@gmail.com>
2022-11-19 14:48:09 +0530

    chore(todo.routes.js): added better comments

--- routes/Todo.route.js
+++ routes/Todo.route.js
@@ -1,15 +1,24 @@
+// Define Router
 const express = require("express");
 const Router = express.Router();
+
+//Imorting all the controllers
 const TodoController = require("../controllers/Todo.controller");
 
+//GET all the tods
 Router.get("/", TodoController.getTodos);
 
+//GET one Todo
 Router.get("/:id", TodoController.getTodo);
 
+//CREATE one todo
 Router.post("/", TodoController.postTodo);
 
+//UPDATE one todo
 Router.put("/:id", TodoController.putTodo);
 
+//DELETE one todo
 Router.delete("/:id", TodoController.deleteTodo);
 
+//Export Router
 module.exports = Router;
