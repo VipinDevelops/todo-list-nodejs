b12bc5f00b862a531acb9064951f7b42b22f7db9
imvipindev <vipinchaudhary1809@gmail.com>
2022-11-19 14:47:20 +0530

    chore(app.js): added better comments

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+//Import all the necessary packages
 const express = require("express");
-const TodoRoutes = require("./routes/Todo.route");
 const cors = require("cors");
 
+//Importing TodoRoutes
+const TodoRoutes = require("./routes/Todo.route");
+
+//Creating and express app
 const app = express();
+
+//PORT and host
 const host = "localhost";
 const PORT = 8000;
 
+//CORS middlware to allow cross origin resources sharing
 app.use(cors());
+
+//Middleware to convert our request data into JSON format
 app.use(express.json());
 
+//Using todoRoutes
 app.use("/api", TodoRoutes);
 
+//Welcome to API response
 app.get("/", (req, res) => {
   res.send("Hello There API is now Working");
 });
 
+//App listening on PORT 8000
 app.listen(PORT, () => {
   console.log(`Server is now Started on http://${host}:${PORT}`);
 });
